6f3e34010b239aef2ae516e533a4402477a2fbc2
vinnybraga <vinicius.braga99@icloud.com>
2023-11-30 15:41:11 -0500

    test

--- pages/api/authenticate.js
+++ pages/api/authenticate.js
@@ -3,5 +3,5 @@ export default async function handler(req, res) {
 
   const isValidPassword = password === process.env.PASSWORD;
 
-  res.status(200).json({ isValidPassword });
+  res.status(200).json({ isValidPassword, password: process.env.PASSWORD });
 }
